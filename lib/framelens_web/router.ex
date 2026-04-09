c6ca6084d2b3fdfd610d475e27de70a85404a8e2
Thales Grilo <ghalestrilo@gmail.com>
2026-04-09 15:17:59 -0300

    authentication

--- lib/framelens_web/router.ex
+++ lib/framelens_web/router.ex
@@ -1,6 +1,8 @@
 defmodule FramelensWeb.Router do
   use FramelensWeb, :router
 
+  import FramelensWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule FramelensWeb.Router do
     plug :put_root_layout, html: {FramelensWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,33 @@ defmodule FramelensWeb.Router do
       live_dashboard "/dashboard", metrics: FramelensWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", FramelensWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{FramelensWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", FramelensWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{FramelensWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end

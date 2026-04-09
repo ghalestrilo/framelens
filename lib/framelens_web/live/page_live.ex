70941b07aad9cc68376339a7a606a1ba53aa83df
Thales Grilo <ghalestrilo@gmail.com>
2026-04-09 14:36:40 -0300

    sync on load

--- lib/framelens_web/live/page_live.ex
+++ lib/framelens_web/live/page_live.ex
@@ -2,55 +2,17 @@ defmodule FramelensWeb.PageLive do
   use FramelensWeb, :live_view
 
   alias Framelens.Scraper
-
-  @hardcoded_videos [
-    %{
-      title: "How to Build a Phoenix App",
-      author_handle: "@elixir_dev",
-      youtube: "https://youtube.com/watch?v=abc123",
-      facebook: "https://facebook.com/video/123456",
-      tiktok: "https://tiktok.com/@user/video/789012",
-      instagram: "https://instagram.com/p/abc123"
-    },
-    %{
-      title: "Elixir GenServers Explained",
-      author_handle: "@functional_programmer",
-      youtube: "https://youtube.com/watch?v=def456",
-      facebook: nil,
-      tiktok: "https://tiktok.com/@user/video/345678",
-      instagram: "https://instagram.com/p/def456"
-    },
-    %{
-      title: "Phoenix LiveView Tutorial",
-      author_handle: "@realtime_web",
-      youtube: "https://youtube.com/watch?v=ghi789",
-      facebook: "https://facebook.com/video/789012",
-      tiktok: "https://tiktok.com/@user/video/901234",
-      instagram: nil
-    },
-    %{
-      title: "Building Scalable Apps with OTP",
-      author_handle: "@elixir_dev",
-      youtube: "https://youtube.com/watch?v=jkl012",
-      facebook: "https://facebook.com/video/345678",
-      tiktok: nil,
-      instagram: "https://instagram.com/p/jkl012"
-    },
-    %{
-      title: "Ecto Query Deep Dive",
-      author_handle: "@database_guru",
-      youtube: nil,
-      facebook: "https://facebook.com/video/901234",
-      tiktok: "https://tiktok.com/@user/video/567890",
-      instagram: "https://instagram.com/p/mno345"
-    }
-  ]
-
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, videos: @hardcoded_videos, syncing: false)}
+    send(self(), :do_sync)
+    {:ok, assign(socket, videos: [], syncing: true)}
   end
 
   def handle_event("sync", _params, socket) do
+    send(self(), :do_sync)
+    {:noreply, assign(socket, syncing: true)}
+  end
+
+  def handle_info(:do_sync, socket) do
     videos = Scraper.sync()
     {:noreply, assign(socket, videos: videos, syncing: false)}
   end

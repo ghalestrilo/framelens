dc9fb43fdd122907066e87b5ef947c464e48821a
Thales Grilo <ghalestrilo@gmail.com>
2026-04-24 00:09:06 -0300

    remove header

--- lib/framelens_web/components/layouts.ex
+++ lib/framelens_web/components/layouts.ex
@@ -35,6 +35,7 @@ defmodule FramelensWeb.Layouts do
 
   def app(assigns) do
     ~H"""
+    <!--
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
@@ -58,6 +59,7 @@ defmodule FramelensWeb.Layouts do
         </ul>
       </div>
     </header>
+  -->
 
     <main class="px-4 py-20 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-2xl space-y-4">

86145f55083065248c9125404b080adb2bcc6991
Thales Grilo <ghalestrilo@gmail.com>
2026-04-27 16:44:31 -0300

    remove dbg

--- lib/framelens/platform/facebook.ex
+++ lib/framelens/platform/facebook.ex
@@ -7,12 +7,10 @@ defimpl Framelens.Platform, for: Framelens.Platform.Facebook do
     base = Application.fetch_env!(:framelens, :rsshub_url)
     url = "#{base}/facebook/page/#{id}"
 
-    dbg(url)
-
     with {:ok, feed} <- ElixirRss.fetch_and_parse(url) do
       {:ok, Enum.map(feed.entries, &Map.put(&1, :author, name))}
     else
-      error -> dbg(error)
+      error -> error
     end
   end
 end
--- lib/framelens/platform/instagram.ex
+++ lib/framelens/platform/instagram.ex
@@ -6,7 +6,6 @@ defimpl Framelens.Platform, for: Framelens.Platform.Instagram do
   def fetch_content(%{platform_id: id, name: name}) do
     base = Application.fetch_env!(:framelens, :rsshub_url)
     url = "#{base}/picnob.info/user/#{id}"
-    dbg(url)
 
     with {:ok, feed} <- ElixirRss.fetch_and_parse(url) do
       {:ok, Enum.map(feed.entries, &Map.put(&1, :author, name))}

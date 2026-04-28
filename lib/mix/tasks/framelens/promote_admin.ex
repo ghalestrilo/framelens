7d721da08d87d046cf02ea95a52adb5fcbb2d9cf
Thales Grilo <ghalestrilo@gmail.com>
2026-04-28 10:07:16 -0300

    create promote admin

---
+++ lib/mix/tasks/framelens/promote_admin.ex
@@ -0,0 +1,26 @@
+defmodule Mix.Tasks.Framelens.PromoteAdmin do
+  use Mix.Task
+
+  @shortdoc "Promotes a user to admin by email"
+
+  @impl Mix.Task
+  def run([email]) do
+    Mix.Task.run("app.start")
+
+    case Framelens.Accounts.get_user_by_email(email) do
+      nil ->
+        Mix.shell().error("No user found with email: #{email}")
+
+      user ->
+        user
+        |> Ecto.Changeset.change(role: "admin")
+        |> Framelens.Repo.update!()
+
+        Mix.shell().info("#{email} promoted to admin")
+    end
+  end
+
+  def run(_) do
+    Mix.shell().error("Usage: mix framelens.promote_admin <email>")
+  end
+end
